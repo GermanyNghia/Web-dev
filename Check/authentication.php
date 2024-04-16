b314be3a7b960d2a3c47041e1706ad568f5664ee
TRAN DUC NGHIA <111063228+GermanyNghia@users.noreply.github.com>
2024-04-16 18:28:10 +0700

    Create authentication.php

---
+++ Check/authentication.php
@@ -0,0 +1,12 @@
+<?php
+
+function end_Section(){
+    session_destroy();
+    header("Location: ../login.php");
+}
+// $_COOKIE["users"] = "teachers";
+$_COOKIE["users"] = "students";
+if (!isset($_SESSION[$_COOKIE["users"]]) || !isset($_SESSION["username"])) {
+    end_Section();
+}
+?>
