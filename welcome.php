69aa5dba57b4a9972498cb1383545bb2b3cf6ae8
TRAN DUC NGHIA <111063228+GermanyNghia@users.noreply.github.com>
2024-04-16 18:31:11 +0700

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="CSS/back.css">
+    <title>My testing website</title>
+</head>
+<body>
+
+    <header>
+        <h1>My school website</h1>
+        <p>Welcome</p>
+    </header>
+
+    <nav class="navbar">
+        <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="#admissions">Admissions</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="login">Sign in</a></li>
+            
+        </ul>
+    </nav>
+
+    <main>
+
+        <section id="about">
+            <h2>About Us</h2>
+            <p>This is my first website</p>
+            <br>
+            <p>Đây là website đầu của tôi</p>
+        </section>
+
+        <section id="admissions">
+            <h2>Admissions</h2>
+            <p>This is not the time for new students</p>
+            <br>
+            <p>Đây không phải thời gian để tuyển sinh</p>
+        </section>
+
+        <section id="contact">
+            <h2>Contact</h2>
+            <p>If you don't have a account, please contact admin</p>
+            <br>
+            <p>Nếu bạn chưa có tài khoản, hãy liên hệ cho admin</p>
+        </section>
+
+        <section id="sign in">
+            <h2>Sign in</h2>
+            <p>If you have a account, you can sign</p>
+            <br>
+            <p>Nếu bạn có tài khoản, bạn có thể đăng nhập</p>
+        </section>
+
+    </main>
+
+    <footer>
+        <p>&copy; 2024 My school website. All rights reserved.</p>
+    </footer>
+
+</body>
+</html>
