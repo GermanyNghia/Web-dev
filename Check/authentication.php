c9e3efe5440e52d448d45102a823ee1a77d61de4
TRAN DUC NGHIA <111063228+GermanyNghia@users.noreply.github.com>
2024-04-22 14:48:48 +0700

    Update authentication.php

--- Check/authentication.php
+++ Check/authentication.php
@@ -4,8 +4,8 @@ function end_Section(){
     session_destroy();
     header("Location: ../login.php");
 }
-// $_COOKIE["users"] = "teachers";
-$_COOKIE["users"] = "students";
+$_COOKIE["users"] = "teachers";
+// $_COOKIE["users"] = "students";
 if (!isset($_SESSION[$_COOKIE["users"]]) || !isset($_SESSION["username"])) {
     end_Section();
 }
